c1a4d4610f949e829b56eac87727d36fdc81e2c5
JohndelVillanueva <Johndel0307@gmail.com>
2024-06-21 09:20:51 +0800

    update commit

--- wispay/processmoney.php
+++ wispay/processmoney.php
@@ -1,43 +1,52 @@
 <?php
-include_once ("config/config.php");
+include_once("config/config.php");
 session_start();
-if(!isset($_SESSION["username"])) {
+
+// Redirect to index if not logged in
+if (!isset($_SESSION["username"])) {
     header("location:index.php");
+    exit();
 }
+
 date_default_timezone_set("Asia/Manila");
 $refcode = uniqid("WIS-");
 
-$statement = $DB_con->prepare('INSERT INTO wispay (debit, credit, rfid, refcode, transdate, processedby) VALUES (:debit, :credit, :rfid, :refcode, :transdate, :processedby)');
+// Insert transaction into the database
+$statement = $DB_con->prepare(
+    'INSERT INTO wispay (debit, credit, rfid, refcode, transdate, processedby)
+     VALUES (:debit, :credit, :rfid, :refcode, :transdate, :processedby)'
+);
+
 $statement->execute([
     'debit' => '0',
     'credit' => $_POST['amount'],
     'rfid' => $_POST['rfid'],
     'refcode' => $refcode,
     'transdate' => date('Y-m-d H:i:s'),
-    'processedby' => $_SESSION['fname']." ".$_SESSION['lname']
+    'processedby' => $_SESSION['fname'] . " " . $_SESSION['lname']
 ]);
 
+// Retrieve student information based on RFID
 $getstudent = $DB_con->prepare("SELECT * FROM user WHERE rfid = :rfid");
-$getstudent->execute([
-        'rfid' => $_POST["rfid"]
-]);
+$getstudent->execute(['rfid' => $_POST["rfid"]]);
 $students = $getstudent->fetchAll();
 
-foreach($students as $row) {
+foreach ($students as $row) {
 ?>
     <meta http-equiv="refresh" content="2; url=showhistory.php?success=1&rfid=<?php echo $_POST['rfid']; ?>&refcode=<?php echo $refcode; ?>" />
-    <div style="width:100%;text-align: center;margin-top:-10px;font-family: 'Friz Quadrata Std Medium', sans-serif">
-    <h3>WISPay RELOAD</h3>
-    <h4>Reference Code<br><?php echo $refcode;?></h4>
-            <?php echo $row["fname"]." ".$row["lname"];?><br>
-            Date : <?php echo date('m/d/Y');?><br>
-
-        <h2 style="font-weight: bold">P <?php echo $_POST['amount'].".00";?></h2><br><hr>
-        <div>
+    <div style="width:100%; text-align:center; margin-top:-10px; font-family: 'Friz Quadrata Std Medium', sans-serif">
+        <h3>WISPay RELOAD</h3>
+        <h4>Reference Code<br><?php echo $refcode; ?></h4>
+        <?php echo $row["fname"] . " " . $row["lname"]; ?><br>
+        Date : <?php echo date('m/d/Y'); ?><br>
+        <h2 style="font-weight: bold">P <?php echo $_POST['amount'] . ".00"; ?></h2><br>
+        <hr>
     </div>
     <script type="text/javascript">
-            window.print();
+        window.print();
     </script>
 <?php
 }
+
 die();
+?>
