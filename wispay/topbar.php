90fe421fe6382c8541dee4811a5e505115f1da95
JohndelVillanueva <Johndel0307@gmail.com>
2024-08-13 08:52:39 +0800

    update commit

--- wispay/topbar.php
+++ wispay/topbar.php
@@ -1,3 +1,12 @@
+<style>
+  .circle-img {
+    width: 100px; /* Adjust the size as needed */
+    height: 100px; /* Adjust the size as needed */
+    border-radius: 50%;
+    object-fit: cover; /* Ensures the image covers the area while maintaining aspect ratio */
+  }
+</style>
+
 <nav class="navbar navbar-expand navbar-light navbar-bg">
     <a class="sidebar-toggle js-sidebar-toggle">
         <i class="hamburger align-self-center"></i>
@@ -11,7 +20,8 @@
                 </a>
 
                 <a class="nav-link dropdown-toggle d-none d-sm-inline-block" href="#" data-bs-toggle="dropdown">
-                    <img src="images/people/<?php echo $_SESSION['photo']; ?>" class="avatar img-fluid rounded me-1" alt="<?php echo $_SESSION['fname']." ".$_SESSION['lname']; ?>" /> <span class="text-dark"><?php echo $_SESSION['fname']." ".$_SESSION['lname']; ?></span>
+                <img src="images/people/<?php echo $_SESSION['photo']; ?>.jpg" class="circle-img me-1" alt="<?php echo $_SESSION['fname'] . ' ' . $_SESSION['lname']; ?>" />
+
                 </a>
                 <div class="dropdown-menu dropdown-menu-end">
                     <a class="dropdown-item" href="logout.php"><i class="align-middle" data-feather="log-out"></i> Log out</a>
