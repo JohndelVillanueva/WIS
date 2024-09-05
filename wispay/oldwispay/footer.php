429fe0565485543764f96cf1d5b1265d962a63ed
JohndelVillanueva <Johndel0307@gmail.com>
2024-09-05 10:21:00 +0800

    delete old wispay

--- wispay/oldwispay/footer.php
+++
@@ -1,8 +0,0 @@
-<!-- jQuery -->
-<script src="js/jquery.min.js"></script>
-
-<!-- Bootstrap Core JavaScript -->
-<script src="js/bootstrap.js"></script>
-<script src="js/bootstrap-datepicker.min.js"></script>
-</body>
-</html>
