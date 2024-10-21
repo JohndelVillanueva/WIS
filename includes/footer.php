77893c6db5c6ed55b82743fb7cf0bcbc67888f51
JohndelVillanueva <Johndel0307@gmail.com>
2024-10-21 16:58:56 +0800

    update commit

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 <footer class="footer">
     <div class="footer-content">
-        <p class="m-b-0">Copyright by: <span style="font-weight:bold;">Chester Sigua</span> Improving by <span style="font-weight:bold;">Johndel Villanueva</span>. All rights reserved.</p>
+        <p class="m-b-0">Copyright by: <span style="font-weight:bold;"><span style="font-weight:bold;">Johndel Villanueva</span>. All rights reserved.</p>
         <span>
                             <a href="https://westfields.edu.ph" class="text-gray m-r-15">Westfields International School</a>
                             <a href="https://fb.me/WestfieldsInternationalSchool" class="text-gray">Facebook</a>
